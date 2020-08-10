3ea1f36e6262344dee17448982eecc51f332a67e
Xiaoliang Li <xiaoliang.li@lixinchuxing.com>
2020-08-10 19:32:43 +0800

    完成用例模块序列化器设计及 retrieve接口的设计组装

---
+++ utils/validates.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from projects.models import Projects
+from interfaces.models import Interfaces
+
+def is_exist_project_id(value):
+    if not Projects.objects.filter(id=value).exist():
+        raise serializers.ValidationError('项目id不存在')
+
+def is_exist_interface_id(value):
+    if not Interfaces.objects.filter(id=value).exist():
+        raise serializers.ValidationError('接口id不存在')
