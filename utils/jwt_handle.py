0e9679ae94bb4c456e51e19ff5670ff287b0c6a7
Xiaoliang Li <xiaoliang.li@lixinchuxing.com>
2020-08-20 20:58:26 +0800

    修改登录更新last——login时间

--- utils/jwt_handle.py
+++ utils/jwt_handle.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-
+import datetime
 
 
 def jwt_response_payload_handler(token, user=None, request=None):
+    user.last_login = str(datetime.datetime.today())
+    user.save()
     return {
         'user_id': user.id,
         'username': user.username,
