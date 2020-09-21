80547e5a5b5c241559abb7f60b964ed18f8af54b
Xiaoliang Li <xiaoliang.li@lixinchuxing.com>
2020-09-21 17:37:43 +0800

    新增推送微信机器人消息模块

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -13,7 +13,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework import permissions
 from rest_framework.response import Response
-
+from rest_framework.viewsets import ViewSetMixin
 from .models import Projects
 from interfaces.models import Interfaces
 from testsuits.models import Testsuits
